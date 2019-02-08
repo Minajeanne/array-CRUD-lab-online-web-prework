925e98a6ef310fda752679cc4602633a7b487b01
Alicia Santiago <albrown07@mac.com>
2019-02-08 20:11:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,25 +3,19 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_array = ["Ellie", "Mina", "Sydney", "Rilee"]
+  my_dogs = ["Ellie", "Mina", "Sydney", "Rilee"]
 end
 
 def add_element_to_end_of_array(array, element)
-   my_dogs = ["Ellie", "Mina", "Sydney", "Rilee"]
-   my_dogs << "Rascal"
+  
 end
 
 def add_element_to_start_of_array(array, element)
-   my_dogs = ["Ellie", "Mina", "Sydney", "Rilee"]
-   my_dogs << "Rascal"
-   my_dogs.unshift("Fido")
+   
 end
 
 def remove_element_from_end_of_array(array)
-   my_dogs = ["Ellie", "Mina", "Sydney", "Rilee"]
-   my_dogs << "Rascal"
-   my_dogs.unshift("Fido")
-   my_dogs.pop
+   
 end
 
 def remove_element_from_start_of_array(array)
